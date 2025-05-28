00d217f649e7458dd967a844ed953409addac92e
Radek Bartoň <radek.barton@microsoft.com>
2025-05-28 12:37:04 +0200

    Workarounds needed to run `winsup` testsuite on the CI with WSL runner (#18)

--- winsup/testsuite/cygrun.sh
+++ winsup/testsuite/cygrun.sh
@@ -13,7 +13,10 @@ then
     windows_runtime_root=$(cygpath -m $runtime_root)
     $mingwtestdir/cygrun "$exe -v -cygwin $windows_runtime_root/cygwin1.dll"
 else
-    # Removing cygdrop $cygrun to make the tests pass while testing on wsl-env
-    WSLENV="$WSLENV:PATH/p" \
-    $exe
+    if uname | grep -qi cygwin; then
+        cygdrop $mingwtestdir/cygrun $exe
+    else
+        # Running under WSL.
+        timeout --preserve-status 300 "$exe"
+    fi
 fi
